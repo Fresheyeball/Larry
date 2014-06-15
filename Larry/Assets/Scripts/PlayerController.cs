434fb72c1a6f1a2ff4279e1291fa0d2d4adefd9f
Isaac Shapira <fresheyeball@gmail.com>
2014-06-15 17:16:19 -0600

    speedy

---
+++ Larry/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void Awake(){
+		rigidbody.freezeRotation = true;
+	}
+
+	void FixedUpdate(){
+		float rotation     = Input.GetAxis ("Horizontal");
+		rigidbody.AddForce (transform.forward * getSpeed());
+	}
+
+	float getSpeed(){
+		float acceleration = Input.GetAxis ("Vertical");
+		Debug.Log (acceleration);
+		if (acceleration > 0f) {
+			return acceleration * speed;
+		} else {
+			return 0f;
+		}
+	}
+
+}
